fa712d31606de5c4c5e8eb583797e08fe6b8e914
amireldb <eldeebamir6@gmail.com>
2025-06-22 05:18:45 +0300

    Create bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount :
+    def __init__(self,account_balance=0):
+
+        self.account_balance=account_balance
+        pass
+    def deposit(self,amount):
+        self.account_balance += amount
+    def withdraw(self,amount):
+        if amount <= self.account_balance:
+            self.account_balance -=amount 
+            return True
+        else :
+            return False
+    def display(self): 
+        print(f"Current Balance: ${self.account_balance}")
+
+ 
