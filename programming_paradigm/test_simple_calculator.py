db0bee7b89f9ee4c11011886d354c413317faa67
amireldb <eldeebamir6@gmail.com>
2025-06-22 06:35:54 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,7 +11,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5,3),2)
     def test_multiplication(self):
         self.assertEqual(self.calc.multiply(5,5),25)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(5,5),1)
             
 
