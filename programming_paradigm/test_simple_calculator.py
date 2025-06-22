046d8295ee4d3ec38de41a509914200f74c45ef4
amireldb <eldeebamir6@gmail.com>
2025-06-22 06:31:15 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,1 +1,18 @@
+import unittest
+from simple_calculator import SimpleCalculator 
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc= SimpleCalculator
+    def test_addition(self):
+        self.assertEqual(self.calc.add(5,3),8)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+    def test_sub(self):
+        self.assertEqual(self.calc.subtract(5,3),2)
+    def test_mul(self):
+        self.assertEqual(self.calc.multiply(5,5),25)
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(5,5),1)
+            
+
 
