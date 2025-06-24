df9c25e21fc4f18a7699964d7e7b7297d6211400
amireldb <eldeebamir6@gmail.com>
2025-06-25 02:43:55 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,9 +5,9 @@ class Book:
     def type(self):
         return 1
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}"
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"Book('{self.title}', '{self.author}')"
 class EBook(Book):
     def __init__(self, title, author,file_size):
         super().__init__(title, author)
@@ -33,10 +33,10 @@ class Library():
         #     print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
     def list_books(self):
         for book in self.books :
-            if book.type == 1 :
+            if book.type() == 1 :
                 print(f"Book: {book.title} by {book.author}")
-            if book.type == 2 :
+            if book.type() == 3 :
                 print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}")
-            if book.type == 1 :
+            if book.type() == 2 :
                 print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
 
