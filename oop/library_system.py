f4cf95a362eb331a3a5b181ba4e79f96d9cc8a5d
amireldb <eldeebamir6@gmail.com>
2025-06-25 02:38:30 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,8 +4,6 @@ class Book:
         self.title = title
     def type(self):
         return 1
-    def __del__(self):
-        print(f"Deleting {self.title}")
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     def __repr__(self):
